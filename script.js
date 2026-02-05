f6433b6afc9b9fc6cade7fec01644c108a35bc69
kaczkakarolinka <52485197+kaczkakarolinka@users.noreply.github.com>
2026-02-05 20:17:19 +0100

    Pierwszy szkic
    Pierwszy yglad strony, bez mobile i bez designu

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var hamburger = document.querySelector('.hamburger');
+  var menu = document.querySelector('.menu');
+
+  hamburger.onclick = function () {
+    menu.classList.toggle('active')};
+
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active1");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+  })
